632b47fe33fb3a842229ee0fe74fcfa4ce0676a9
wangxs2 <15037589151@163.com>
2020-02-27 19:43:21 +0800

    实时播报新增、编辑、删除接口联调

--- src/views/curTimeInfo/curTime.vue
+++ src/views/curTimeInfo/curTime.vue
@@ -32,6 +32,7 @@
           </el-select>
         </div>
         <div class="search-input search-btn" @click="search">搜索</div>
+        <div class="search-input search-btn" @click="add">新增</div>
 
       </div>
       <div class="table-wrapper">
@@ -61,6 +62,15 @@
               <div style="padding:5px;" v-if="scope.row.isTop==0">否</div>
             </template>
           </el-table-column>
+          <el-table-column
+            fixed="right"
+            label="操作"
+            width="100">
+            <template slot-scope="scope">
+              <el-button @click="editRow(scope.row)" type="text" size="small">编辑</el-button>
+              <el-button @click="deleteRow(scope.row)" type="text" size="small">删除</el-button>
+            </template>
+          </el-table-column>
         </el-table>
         <div class="block">
           <el-pagination
@@ -76,6 +86,36 @@
         </div>
       </div>
     </div>
+
+    <el-dialog :title="addOrEditPoint==1?'编辑':'新增'" :visible.sync="dialogVisible" :close-on-click-modal="false">
+      
+      <el-form label-position="right" :model="form" :rules="rules" ref="form" >
+        <el-form-item label="标题" prop="headline">
+          <el-input v-model="form.headline" placeholder="请输入标题"></el-input>
+        </el-form-item>
+        <el-form-item label="发布时间" prop="pubDate">
+          <el-date-picker v-model="form.pubDate" type="datetime" placeholder="选择发布时间" format="yyyy-MM-dd hh:mm:ss">
+          </el-date-picker>
+        </el-form-item>
+        <el-form-item label="消息主体" prop="mainBody">
+          <el-input v-model="form.mainBody" type="textarea" :rows="5" placeholder="请输入消息主体"></el-input>
+        </el-form-item>
+        <el-form-item label="信息来源" prop="publishSource">
+          <el-input v-model="form.publishSource" placeholder="请输入信息来源"></el-input>
+        </el-form-item>
+        <el-form-item label="网页链接" prop="url">
+          <el-input v-model="form.url" placeholder="请输入网页链接"></el-input>
+        </el-form-item>
+        <el-form-item label="是否置顶" prop="isTop">
+          <el-radio-group v-model="form.isTop">
+            <el-radio v-for="item in isTopList" :key="item.type" :label="item.type">{{item.name}}</el-radio>
+          </el-radio-group>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="submitForm('form')">提交</el-button>
+        </el-form-item>
+      </el-form>
+    </el-dialog>
 
   </div>
 </template>
@@ -113,6 +153,48 @@ export default {
       gridData: [],
       params:{},
       pageshow:true,
+
+      dialogVisible:false,
+      addOrEditPoint:0,
+      curId:'',
+      isTopList:[
+        {
+          type:1,
+          name:"是"
+        },
+        {
+          type:0,
+          name:"否"
+        }
+      ],
+      form:{
+        headline:'', // 标题
+        pubDate:'', // 发布时间
+        mainBody:'', // 消息主体
+        publishSource:'', // 信息来源
+        url:'', // 网页链接
+        isTop:1, // 是否置顶
+      },
+      rules: {
+        headline: [
+          { required: true, message: '请输入标题', trigger: 'blur' },
+        ],
+        pubDate: [
+          { required: true, message: '请选择发布时间', trigger: 'change' },
+        ],
+        mainBody: [
+          { required: true, message: '请输入消息主体', trigger: 'blur' },
+        ],
+        publishSource: [
+          { required: true, message: '请输入信息来源', trigger: 'blur' },
+        ],
+        url: [
+          { required: true, message: '请输入网页链接', trigger: 'blur' },
+        ],
+        isTop: [
+          { required: true, message: '请选择是否置顶', trigger: 'change' },
+        ],
+      },
     }
   },
   mounted () {
@@ -129,6 +211,90 @@ export default {
     this.getNeedsNameList()
   },
   methods: {
+    add(){
+      this.addOrEditPoint=0
+      this.dialogVisible=true
+      this.form={
+        headline:'',
+        pubDate:'',
+        mainBody:'',
+        publishSource:'',
+        url:'',
+        isTop:1,
+      }
+    },
+    editRow(row){
+      this.addOrEditPoint=1
+      this.dialogVisible=true
+      this.curId=row.id
+      this.form={
+        headline:row.headline,
+        pubDate:row.pubDate,
+        mainBody:row.mainBody,
+        publishSource:row.publishSource,
+        url:row.url,
+        isTop:row.isTop,
+      }
+    },
+    deleteRow(row){
+      this.$fetchPost("donate/delete",{id:row.id}).then(res => {
+        this.$message({
+          message: res.message,
+          type: 'success'
+        });
+        if (res.result==1){
+          this.regetList()
+        }
+      })
+    },
+    submitForm(form){
+        this.$refs[form].validate((valid) => {
+          if (valid) {
+            
+            if (this.addOrEditPoint==0){
+              this.$fetchPost("donate/insert",this.form,"json").then(res => {
+                this.$message({
+                  message: res.message,
+                  type: 'success'
+                });
+                if (res.result==1){
+                  this.dialogVisible=false
+                  this.regetList()
+                }
+              })
+            }else if (this.addOrEditPoint==1){
+              this.form.id=this.curId
+              this.$fetchPost("donate/update",this.form,"json").then(res => {
+                this.$message({
+                  message: res.message,
+                  type: 'success'
+                });
+                if (res.result==1){
+                  this.dialogVisible=false
+                  this.regetList()
+                }
+              })
+            } 
+          } else {
+            console.log('error submit!!');
+            return false;
+          }
+        });
+    },
+    // 操作完成获取数据
+    regetList(){
+      this.pageshow = false
+      this.page=1
+      this.tableData=[]
+      this.params={
+        page:this.page,
+        pageSize:this.pageSize
+      }
+      this.getTableData(this.params)
+      this.$nextTick(() => {
+          this.pageshow = true
+      })
+    },
     getNeedsNameList(){
       this.$fetchGet("donate/getHeadline",{
         tab:'supply'
@@ -233,6 +399,17 @@ export default {
 .cur-time{
   .el-date-editor.el-input, .el-date-editor.el-input__inner{
     width:350px !important;
+  }
+  .el-form{
+
+    .el-form-item{
+      display:flex;
+      justify-content:flex-start;
+      align-items:center;
+    }
+    .el-input,.el-form-item__content{
+      width:600px;
+    }
   }
   
 }
